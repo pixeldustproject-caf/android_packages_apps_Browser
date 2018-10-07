BenzoRom/packages_apps_Browser

Browser: add reach mode
Add an option to move address bar to the bottom

Change-Id: I52627b63c5ab24c7c5d989f69a4ec3175d9e329c
Signed-off-by: Joey <joey@lineageos.org>
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

app/src/main/java/org/benzorom/browser/SettingsActivity.java
@@ -16,9 +16,12 @@
 package org.benzorom.browser;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -30,6 +33,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.benzorom.browser.utils.PrefsUtils;
+import org.benzorom.browser.utils.UiUtils;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -66,6 +70,18 @@ public class SettingsActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
                 return true;
             });
+
+            SwitchPreference reachMode = (SwitchPreference) findPreference(("key_reach_mode"));
+            if (UiUtils.isTablet(getContext())) {
+                getPreferenceScreen().removePreference(reachMode);
+            } else {
+                reachMode.setOnPreferenceClickListener(preference -> {
+                    Intent intent = new Intent(MainActivity.EVENT_CHANGE_UI_MODE);
+                    intent.putExtra(MainActivity.EVENT_CHANGE_UI_MODE, reachMode.isChecked());
+                    LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
+                    return true;
+                });
+            }
         }
 
         private void editHomePage(Preference preference) {

app/src/main/java/org/benzorom/browser/utils/UiUtils.java
@@ -15,7 +15,10 @@
  */
 package org.benzorom.browser.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -23,12 +26,18 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.preference.PreferenceManager;
+import android.support.annotation.AttrRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.StyleRes;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
+
+import org.benzorom.browser.R;
 
 public final class UiUtils {
 
@@ -112,5 +121,22 @@ public final class UiUtils {
     public static void setImageButtonEnabled(ImageButton button, boolean enabled) {
         button.setEnabled(enabled);
         button.setAlpha(enabled ? 1.0f : 0.4f);
+    }
+
+    public static boolean isTablet(Context context) {
+        return context.getResources().getBoolean(R.bool.is_tablet);
+    }
+
+    public static boolean isReachModeEnabled(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean("key_reach_mode", false);
+    }
+
+    public static float getDimenAttr(Context context, @StyleRes int style, @AttrRes int dimen) {
+        int[] args = { dimen };
+        TypedArray array = context.obtainStyledAttributes(style, args);
+        float result = array.getDimension(0, 0f);
+        array.recycle();
+        return result;
     }
 }

pixeldustproject-caf/android_packages_apps_Browser

Signed-off-by: spezi77 <spezi7713@gmx.net>

java/com/pixeldust/browser/SettingsActivity.java
@@ -16,9 +16,12 @@
 package com.pixeldust.browser;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -30,6 +33,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.pixeldust.browser.utils.PrefsUtils;
+import com.pixeldust.browser.utils.UiUtils;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -66,6 +70,18 @@ public class SettingsActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
                 return true;
             });
+
+            SwitchPreference reachMode = (SwitchPreference) findPreference(("key_reach_mode"));
+            if (UiUtils.isTablet(getContext())) {
+                getPreferenceScreen().removePreference(reachMode);
+            } else {
+                reachMode.setOnPreferenceClickListener(preference -> {
+                    Intent intent = new Intent(MainActivity.EVENT_CHANGE_UI_MODE);
+                    intent.putExtra(MainActivity.EVENT_CHANGE_UI_MODE, reachMode.isChecked());
+                    LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
+                    return true;
+                });
+            }
         }
 
         private void editHomePage(Preference preference) {

java/com/pixeldust/browser/utils/UiUtils.java
@@ -15,7 +15,10 @@
  */
 package com.pixeldust.browser.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -23,12 +26,18 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.preference.PreferenceManager;
+import android.support.annotation.AttrRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.StyleRes;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
+
+import com.pixeldust.browser.R;
 
 public final class UiUtils {
 
@@ -112,5 +121,22 @@ public final class UiUtils {
     public static void setImageButtonEnabled(ImageButton button, boolean enabled) {
         button.setEnabled(enabled);
         button.setAlpha(enabled ? 1.0f : 0.4f);
+    }
+
+    public static boolean isTablet(Context context) {
+        return context.getResources().getBoolean(R.bool.is_tablet);
+    }
+
+    public static boolean isReachModeEnabled(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getBoolean("key_reach_mode", false);
+    }
+
+    public static float getDimenAttr(Context context, @StyleRes int style, @AttrRes int dimen) {
+        int[] args = { dimen };
+        TypedArray array = context.obtainStyledAttributes(style, args);
+        float result = array.getDimension(0, 0f);
+        array.recycle();
+        return result;
     }
 }